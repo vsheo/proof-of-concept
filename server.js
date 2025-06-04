a6cd0c69dc5d46c4b47962fd72ddb2400ae833c1
vsheo <vireshsheoratan@gmail.com>
2025-06-04 22:12:16 +0200

    names uit fetch url #5

--- server.js
+++ server.js
@@ -11,10 +11,43 @@ app.engine("liquid", engine.express());
 app.set("views", "./views");
 
 
+// oude index GET, het werkt
+// app.get("/", async function (request, response) {
+//     const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+//     const pokemonResponseJSON = await pokemon.json();
+
+//     // const pkmId = pokemonResponseJSON.filter((name) =>
+//     //     pkmInfo = getPkmInfo(name)
+//     // );
+
+//     response.render("index.liquid", { pokeman: pokemonResponseJSON.results,  });
+// });
+
+
+
 // index GET
 app.get("/", async function (request, response) {
-    response.render("index.liquid");
+    const ffff = getPkmData()
+
+    response.render("index.liquid", { pokeman: ffff });
 });
+
+
+
+// get all names
+async function getPkmData() {
+    // deze link, + de name heeft de id van de pokemon
+	const indexDataResp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+	const indexDataRespJSON = await indexDataResp.json();
+    const pkmNameUrl = indexDataRespJSON.results
+
+    // gebruik map om de name van elke pokemon op te slaan in een list
+    const nameList = pkmNameUrl.map(pokemon => pokemon.name);
+
+	return nameList;
+}
+
+
 
 // detail GET
 app.get("/detail", async function (request, response) {
