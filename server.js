94e06d0231d8165e935f0dcad495978e1ccc77c4
vsheo <vireshsheoratan@gmail.com>
2025-06-05 14:15:37 +0200

    cache.json #5

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ async function getIndexData() {
 
     // https://hostman.com/tutorials/how-to-use-javascript-array-map/#example-3--applying-discounts-to-products
     // gebruik map op een fetch te doen op elke url in pkmURL
-    const typePromises = pkmURLs.map(async (detailURL, index) => {
+    const fetchPromises = pkmURLs.map(async (detailURL, index) => {
         const pkmDetailResp = await fetch(detailURL)
         const pkmDetails = await pkmDetailResp.json()
         
@@ -51,17 +51,32 @@ async function getIndexData() {
         // hiermee kan ik javascript object terug geven, die ik met JSON.stringify tot JSON kan maken
         return  {
             id: index + 1,
-            name: pkmName,
             types
         };
     })
 
     // https://hostman.com/tutorials/how-to-use-javascript-array-map/#managing-asynchronous-operations
     // de rede waarom mijn json leeg was is omdat ik geen promise.all gebruikt had waardoor de volgende fetch uitgevoerd werd zonder dat de vorige klaar was
-    const pkmTypes = await Promise.all(typePromises)
+    const pkmTypes = await Promise.all(fetchPromises)
+
+    // gebruik de functie om de correcte structuur te maken
+    const jsonStruc = structureJSON(pkmName, pkmTypes)
 
     // zet de structuur om naar JSON en sla het op in de cache
-    fs.writeFileSync('cache.json', JSON.stringify(pkmTypes, null, 2));
+    fs.writeFileSync('cache.json', JSON.stringify(jsonStruc, null, 2));
+}
+
+// deze functie neemt de name, id en types van een pokemon, zet het samen en geeft het terug met een return
+function structureJSON(names, types) {
+    // for (let i = 0; i < types.length; i++) {
+    //     types[i].name1 = names[i]
+    // }
+
+    types.forEach((type, i) => {
+        type.name = names[i]
+    });
+
+    return types
 }
 
 
