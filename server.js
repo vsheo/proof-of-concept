8a4d9ff5f58ab8e5cef8e44e43c4e5bb36e74356
vsheo <vireshsheoratan@gmail.com>
2025-06-05 21:03:57 +0200

    mobile first #2

--- server.js
+++ server.js
@@ -2,77 +2,74 @@ import express from "express"
 import { Liquid } from "liquidjs"
 import fs from "fs"
 
-const app = express();
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
-const engine = new Liquid();
-app.engine("liquid", engine.express());
-app.set("views", "./views");
+const engine = new Liquid()
+app.engine("liquid", engine.express())
+app.set("views", "./views")
 
 // read de cache.json die lokaal staat
-const cacheData = fs.readFileSync('cache.json', 'utf-8');
-const cacheDataJSON = JSON.parse(cacheData);
-
+const cacheData = fs.readFileSync("cache.json", "utf-8");
+const cacheDataJSON = JSON.parse(cacheData)
 
 // index GET
 app.get("/", async function (request, response) {
     // 12 uur in milliseconden
-    const twelveH = 43200000
+    const twelveH = 43200000;
     const now = Date.now();
 
     // https://stackoverflow.com/questions/7559555/last-modified-file-date-in-node-js
     // check de laatste keer dat cache.json bewerkt was
-    var stats = fs.statSync('cache.json');
-    var mtime = stats.mtimeMs;
-    if ((now - mtime) < twelveH) {
-        console.log('cache is new')
-    }
-    else {
+    var stats = fs.statSync("cache.json")
+    var mtime = stats.mtimeMs
+    if (now - mtime < twelveH) {
+        console.log("cache is new")
+    } else {
         getIndexData()
-        console.log('cache geupdate')
+        console.log("cache geupdate")
     }
 
     response.render("index.liquid", { pkmData: cacheDataJSON });
-});
+})
 
 // detail GET
 app.get("/detail", async function (request, response) {
-    response.render("detail.liquid");
+    response.render("detail.liquid")
 });
 
-
 // FUNCTIES
-// get names, id's and types van elke pokemon 
+// get names, id's and types van elke pokemon
 async function getIndexData() {
     // deze url heeft alle pokemon names en een link naar de details van de pokemon waar de types staan
-	const nameURLResp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`)
-	const nameURLRespJSON = await nameURLResp.json()
+    const nameURLResp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`)
+    const nameURLRespJSON = await nameURLResp.json()
     const pkmNameURL = nameURLRespJSON.results
 
     // https://hostman.com/tutorials/how-to-use-javascript-array-map/
     // gebruik map om een lijst met pokemon names te maken
-    const pkmName = pkmNameURL.map(pokemon => pokemon.name)
+    const pkmName = pkmNameURL.map((pokemon) => pokemon.name)
 
     // gebruik map om een lijst van fetch url's te maken
-    const pkmURLs = pkmNameURL.map(pokemon => pokemon.url)
+    const pkmURLs = pkmNameURL.map((pokemon) => pokemon.url)
 
     // https://hostman.com/tutorials/how-to-use-javascript-array-map/#example-3--applying-discounts-to-products
     // gebruik map om een fetch te doen op elke url in pkmURL
     const fetchPromises = pkmURLs.map(async (detailURL, index) => {
         const pkmDetailResp = await fetch(detailURL)
         const pkmDetails = await pkmDetailResp.json()
-        
+
         // gebruik map om uit de pkmDetails de types te halen
-        const types = pkmDetails.types.map((pokemon) => pokemon.type.name)
+        const types = pkmDetails.types.map((pokemon) => pokemon.type.name);
 
         // https://www.javascripttutorial.net/javascript-return-multiple-values/#:~:text=Returning%20multiple%20values%20from%20a%20function%20using%20an%20object
         // hiermee kan ik javascript object terug geven, die ik met JSON.stringify tot JSON kan maken
-        return  {
+        return {
             id: index + 1,
-            types
-        };
+            types,
+        }
     })
 
     // https://hostman.com/tutorials/how-to-use-javascript-array-map/#managing-asynchronous-operations
@@ -83,7 +80,7 @@ async function getIndexData() {
     const jsonStruc = structureJSON(pkmName, pkmTypes)
 
     // zet de structuur om naar JSON en sla het op in de cache
-    fs.writeFileSync('cache.json', JSON.stringify(jsonStruc, null, 2));
+    fs.writeFileSync("cache.json", JSON.stringify(jsonStruc, null, 2))
 }
 
 // deze functie neemt de name, id en types van een pokemon, zet het samen en geeft het terug met een return
@@ -92,10 +89,10 @@ function structureJSON(names, types) {
         type.name = names[i]
     });
 
-    return types
+    return types;
 }
 
-app.set("port", process.env.PORT || 8000);
+app.set("port", process.env.PORT || 8000)
 
 app.listen(app.get("port"), function () {
     console.log(
